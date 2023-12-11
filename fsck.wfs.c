a64ac3db56ed6fe8fc9599caf3ef040e79989b47
Rashed Almansoori <rashed@vm-instunix-12.cs.wisc.edu>
2023-12-10 23:42:43 -0600

    fsck.wfs.c pseudo code done

--- fsck.wfs.c
+++ fsck.wfs.c
@@ -1,3 +1,72 @@
 int main(int argc, char *argv[]) {
     return 0;
 }
+
+
+// Main function
+    // Check if disk path argument is provided
+//    If not enough arguments provided
+//        Print error message and exit
+
+//    disk_path = get disk path from arguments
+
+    // Open the disk file
+//    file_descriptor = open disk file (disk_path)
+
+//    If file_descriptor is invalid
+//        Print error message and exit
+
+    // Get file information for mmap
+//    stat_info = get file information (file_descriptor)
+
+    // Memory map the disk
+//    mapped_disk = memory map the disk (file_descriptor, size from stat_info)
+
+    // Close the file descriptor as it's no longer needed
+//    close file_descriptor
+
+    // Perform file system check and log compaction
+//    PerformFsck(mapped_disk)
+
+    // Unmap the memory-mapped disk
+//    unmap memory (mapped_disk)
+
+//    return success
+
+
+// Function to perform file system check and log compaction
+//Function PerformFsck(mapped_disk)
+    // Initialize necessary variables and structures
+//    Initialize variables for traversal, inode tracking, etc.
+
+    // Iterate over the log entries in the disk
+//    For each log entry in mapped_disk
+        // Check for inconsistencies or errors in the log entry
+//        If log entry is inconsistent or erroneous
+//            Handle or report the inconsistency
+
+        // Check for redundancy
+//        If current log entry is redundant (e.g., older version of an inode)
+//            Mark for compaction or removal
+
+
+    // Compact the log by removing marked redundant entries
+//    CompactLog(mapped_disk)
+
+
+
+// Function to compact the log
+//Function CompactLog(mapped_disk)
+    // Initialize variables for log compaction
+//    Initialize variables for new log, tracking, etc.
+
+    // Iterate over the log entries
+//    For each log entry in mapped_disk
+        // If the entry is not marked for removal
+//        If log entry is not marked for compaction
+//            Copy entry to new log
+
+
+    // Update the log head pointer to the new compacted log
+//    UpdateLogHead(mapped_disk)
+
